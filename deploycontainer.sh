0be4f4490663249bc197401f06644dd6d923cc23
Robbie Minshall <rjminsha@us.ibm.com>
2015-03-27 12:18:58 -0400

    opps

--- deploycontainer.sh
+++ deploycontainer.sh
@@ -14,6 +14,8 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #********************************************************************************
+terrible mistake 
+
 
 dump_info () {
     echo -e "${label_color}Container Information: ${no_color}"
